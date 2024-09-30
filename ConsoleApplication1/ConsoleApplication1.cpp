f14e3bb4af0b84de75dd358bb2076567686149ae
anastasiashklyar <rm5rm7j@gmail.com>
2024-09-30 18:09:41 +0700

    5 на 5

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -91,7 +91,7 @@ unsigned countValueInCol(int** mas, unsigned n, unsigned m, int value, unsigned 
 // основная программа
 int main()
 {
-	int** mas = nullptr, n = 4, m = 4;
+	int** mas = nullptr, n = 5, m = 5;
 
 	createMatrix(&mas, n, m);
 	inputMatrix(&mas, n, m);
